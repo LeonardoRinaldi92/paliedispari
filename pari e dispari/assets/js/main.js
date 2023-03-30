dc8b80481091939d802107d19548b90df631ebf0
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-03-30 15:20:34 +0200

    STRUTTURA NUMERI

--- pari e dispari/assets/js/main.js
+++ pari e dispari/assets/js/main.js
@@ -2,20 +2,25 @@ let btnvai = document.getElementById("btnVai");
 let numero  ;
 let scegli ;
 let sceltanumero = document.getElementById("numero");
+let numeroCPU ;
 
 sceltanumero.addEventListener("change", function() {
     numero = parseInt(document.getElementById("numero").value)
     console.log(numero)
     if (numero > 5) {
-        alert ("ti avevo detto non piu di 5!!")
+        alert ("ti avevo detto non piu di 5!!" + " " + numero + " " + "è" + " " + (numero - 5) + " " +"in più di 5...")
         document.getElementById("numero").value = 5
 
     } else if (numero == 0) {
         alert ("ho capito che tra 1 e 5 è nosioso... ma 0 è inappropriato!")
         document.getElementById("numero").value = 1
-    } 
+    } else if (numero <= -273) {
+        alert ("siamo oltre lo 0 assoluto,ATTENTEZIONE GLI ATOMI SI SFALDANO...ADIOS!")
+        document.getElementById("numero").value = 1
+
+    }
     else if (numero < 1) {
-        alert ("esagerato! fa freddo sotto lo 0?")
+        alert ("esagerato"+ " " + numero + "! fa freddo sotto lo 0?")
         document.getElementById("numero").value = 1
     }    
 })
@@ -26,6 +31,9 @@ btnvai.addEventListener("click", function() {
 
     numero = parseInt(document.getElementById("numero").value)
     scegli = document.getElementById("scelta").value
+    numeroCPU = 
+
+
 
 
  
